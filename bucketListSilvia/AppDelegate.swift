8552323b39edb3f5f55032a3565323712cd8a12e
Ben Smith <benjaminsmith1981@gmail.com>
2017-03-31 10:22:32 +0200

    test

--- bucketListSilvia/AppDelegate.swift
+++ bucketListSilvia/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
+//adding  test comment
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         FIRApp.configure()
